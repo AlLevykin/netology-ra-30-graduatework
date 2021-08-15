4e43edfbebfe632ca2a88f46e114b8fa70f02eb4
AlLevykin <a.a.levykin@gmail.com>
2021-08-15 14:03:04 +0500

    Рефакторинг CSS

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -2,7 +2,7 @@ import image from '../img/banner.jpg';
 
 const Banner = () =>
     <div className="banner">
-        <img className="img-fluid" src={image} alt="К весне готовы!" />
+        <img className="w-100 h-auto" src={image} alt="К весне готовы!" />
         <h2 className="banner-header">К весне готовы!</h2>
     </div>;
 
