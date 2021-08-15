24ef2eb7be9cb8de79877ebe0145e70a032d4704
AlLevykin <a.a.levykin@gmail.com>
2021-08-15 14:09:03 +0500

    Исправлена верстка баннера

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -3,7 +3,7 @@ import image from '../img/banner.jpg';
 const Banner = () =>
     <div className="banner">
         <img className="w-100 h-auto" src={image} alt="К весне готовы!" />
-        <h2 className="banner-header">К весне готовы!</h2>
+        <div className="banner-header">К весне готовы!</div>
     </div>;
 
 export default Banner;
