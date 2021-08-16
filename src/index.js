a210b2543ee9fa6fa48ee16b63cd3cd7a62ddf4f
AlLevykin <a.a.levykin@gmail.com>
2021-08-17 00:37:53 +0500

    Настройка обработки ошибок при загрузке данных с сервера

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { stopReportingRuntimeErrors } from "react-error-overlay";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import './index.css';
+
+stopReportingRuntimeErrors();
 
 ReactDOM.render(
   <React.StrictMode>
