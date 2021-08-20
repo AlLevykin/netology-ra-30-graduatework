ee982b7acffb992b2ff70659fda8c8e8903197da
AlLevykin <a.a.levykin@gmail.com>
2021-08-20 10:54:15 +0500

    Отображение количества товаров в корзине

--- src/components/Header/CartLink.jsx
+++ src/components/Header/CartLink.jsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
-const CartLink = () =>
-    <div className="header-controls-pic header-controls-cart">
-        <Link className="stretched-link" to="cart.html" title="Корзина">
-            <div className="header-controls-cart-menu"></div>
-        </Link>
-    </div>
+const CartLink = () => {
+
+    const cartFull = useSelector(
+        state => state.order.items.length
+    )
+
+    return (
+        <div className="header-controls-pic header-controls-cart">
+            <Link className="stretched-link" to="cart.html" title="Корзина">
+                <div className="header-controls-cart-menu"></div>
+                {
+                    (cartFull > 0) &&
+                    <div className="header-controls-cart-full">{cartFull}</div>
+                }
+            </Link>
+        </div>
+    );
+}
 
 export default CartLink;
